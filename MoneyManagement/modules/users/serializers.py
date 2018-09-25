76fe00f73c1ef0feb1022f5b755e167ecd5a35a4
C0deZ Lee <c0dezlee@gmail.com>
2018-09-25 11:38:31 -0400

    Bug fix for missing name for new users

--- MoneyManagement/modules/users/serializers.py
+++ MoneyManagement/modules/users/serializers.py
@@ -21,12 +21,14 @@ class CreateUserSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = User
-		fields = ('id', 'email', 'password',)
+		fields = ('id', 'email', 'password', 'first_name', 'last_name',)
 		extra_kwargs = {'password': {'write_only': True}}
 
 	def create(self, validated_data):
 		user = User(
 			email=validated_data['email'],
+			first_name=validated_data['first_name'],
+			last_name=validated_data['last_name'],
 		)
 		user.set_password(validated_data['password'])
 		user.save()
