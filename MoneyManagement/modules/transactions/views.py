9be58635d26a08ea0ec58806acf8d164872c7a02
Tyler Blankenship <tjbloop@gmail.com>
2018-09-26 17:05:37 -0400

    Exception handling

--- MoneyManagement/modules/transactions/views.py
+++ MoneyManagement/modules/transactions/views.py
@@ -38,25 +38,33 @@ class TransactionViewSet(viewsets.ViewSet):
         if request.method == 'POST':
             user_data = generator.generate_new_user_data(user_type)
             mock_email = user_data[0][1] + user_data[0][2] + "@gmail.com"
-            user = User.objects.create(
-                last_name=user_data[0][1],
-                first_name=user_data[0][2],
-                email=mock_email,
-                password=user_data[0][2],
-                user_type=user_data[0][10],
-                user_id=user_data[0][0]
-            )
-            for transaction in user_data:
-                location = Location.objects.get(vendor_name=transaction[8])
-                day = transaction[3]
-                time = transaction[4]
-                date_object = datetime.strptime(day + " " + time, '%m/%d/%y %H:%M')
-                Transaction.objects.create(
-                    amount=transaction[6],
-                    category=transaction[7],
-                    payment_method=transaction[9],
-                    user=user,
-                    location=location,
-                    sale_date=date_object
+            try:
+                user = User.objects.create(
+                    last_name=user_data[0][1],
+                    first_name=user_data[0][2],
+                    email=mock_email,
+                    password=user_data[0][2],
+                    user_type=user_data[0][10],
+                    user_id=user_data[0][0]
                 )
+            except Exception as e:
+                print(e)
+                return Response("Error creating new user")
+            try:
+                for transaction in user_data:
+                    location = Location.objects.get(vendor_name=transaction[8])
+                    day = transaction[3]
+                    time = transaction[4]
+                    date_object = datetime.strptime(day + " " + time, '%m/%d/%y %H:%M')
+                    Transaction.objects.create(
+                        amount=transaction[6],
+                        category=transaction[7],
+                        payment_method=transaction[9],
+                        user=user,
+                        location=location,
+                        sale_date=date_object
+                    )
+            except Exception as e:
+                print(e)
+                return Response("Error creating transactions")
             return Response("Success")
