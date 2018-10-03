9d42b461057745c0ea340ae3e0dc43d1252da999
Tyler Blankenship <tjbloop@gmail.com>
2018-10-03 17:19:31 -0400

    Monthly Category Spendings API

--- MoneyManagement/modules/transactions/serializers.py
+++ MoneyManagement/modules/transactions/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Transaction
+from .models import Transaction, Location
 
+
+class LocationSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Location
+		fields = '__all__'
 
 class TransactionSerializer(serializers.ModelSerializer):
+	location = LocationSerializer()
 	class Meta:
 		model = Transaction
 		fields = ('user', 'amount', 'category', 'payment_method', 'location', 'sale_date',)
--- MoneyManagement/modules/transactions/urls.py
+++ MoneyManagement/modules/transactions/urls.py
@@ -12,8 +12,18 @@ transaction_retriever = TransactionViewSet.as_view({
 	'get': 'retrieve_user_transactions',
 })
 
+monthly_category_retriever = TransactionViewSet.as_view({
+	'get': 'retrieve_user_monthly_category_spendings',
+})
+
+all_months_category_retriever = TransactionViewSet.as_view({
+	'get': 'retrieve_all_user_monthly_category_spendings',
+})
+
 
 urlpatterns = [
 	path('generate/<user_type>/', transaction_generator, name='transaction_generator'),
-	path('get/<user_id>/', transaction_retriever, name='transaction_retriever')
+	path('get/<user_id>/', transaction_retriever, name='transaction_retriever'),
+	path('get/categories/<user_id>/<month>/<year>/', monthly_category_retriever, name='monthly_category_retriever'),
+	path('get/categories/<user_id>/<category>/', all_months_category_retriever, name='all_months_category_retriever')
 ]
