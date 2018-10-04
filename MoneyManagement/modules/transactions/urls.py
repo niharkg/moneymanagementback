0c3c973d849bf1fac64c5e03a2a6385408517fb3
Tyler Blankenship <tjbloop@gmail.com>
2018-10-04 11:10:21 -0400

    Expansion of API

--- MoneyManagement/modules/transactions/urls.py
+++ MoneyManagement/modules/transactions/urls.py
@@ -25,9 +25,14 @@ transaction_start_date_retriever = TransactionViewSet.as_view({
 })
 
 urlpatterns = [
+	# Generate a new user
 	path('generate/<user_type>/', transaction_generator, name='transaction_generator'),
+	# Get all recent transactions
 	path('get/<user_id>/', transaction_retriever, name='transaction_retriever'),
+	# Get category spending breakdown for a particular month and year
 	path('get/categories/<user_id>/<month>/<year>/', monthly_category_retriever, name='monthly_category_retriever'),
+	# Get a specific or all category breakdown for all active user months
 	path('get/categories/<user_id>/<category>/', all_months_category_retriever, name='all_months_category_retriever'),
+	# Get all transactions after a specific start date
 	path('get/<user_id>/<start_date>/', transaction_start_date_retriever, name='transaction_start_date_retriever'),
 ]
