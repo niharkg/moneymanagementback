8dcce0078e8e0985618311944b2a2c5c6eb14459
C0deZ Lee <c0dezlee@gmail.com>
2018-10-17 22:23:18 -0400

    Modify data generator

--- MoneyManagement/modules/transactions/views.py
+++ MoneyManagement/modules/transactions/views.py
@@ -16,7 +16,7 @@ from MoneyManagement.utils import generator
 
 
 class TransactionViewSet(viewsets.ViewSet):
-    """
+	"""
 	The viewset for company module
 	"""
 	parser_classes = (FormParser, JSONParser)
@@ -25,15 +25,15 @@ class TransactionViewSet(viewsets.ViewSet):
 	def breakdown_monthly_spending(self, transactions):
 		spending_dict = {}
 		for transaction in transactions:
-            month = transaction["sale_date"][0:4]
-            year = transaction["sale_date"][5:7]
-            category = transaction["category"]
-            month_string = month + "/" + year
-            if month_string not in spending_dict:
-                spending_dict[month_string] = {}
-            if category in spending_dict[month_string]:
-                spending_dict[month_string][category] += transaction["amount"]
-            else:
+			month = transaction["sale_date"][0:4]
+			year = transaction["sale_date"][5:7]
+			category = transaction["category"]
+			month_string = month + "/" + year
+			if month_string not in spending_dict:
+				spending_dict[month_string] = {}
+			if category in spending_dict[month_string]:
+				spending_dict[month_string][category] += transaction["amount"]
+			else:
 				spending_dict[month_string][category] = transaction["amount"]
 		return spending_dict
 
@@ -137,45 +137,45 @@ class TransactionViewSet(viewsets.ViewSet):
 			9:  payment type
 			10: user_type
 		"""
-        if request.method == 'POST':
-            user_data = generator.generate_new_user_data(user_type)
-            mock_email = user_data[0][1] + user_data[0][2] + "@gmail.com"
-            try:
-                user = User.objects.create(
-                    last_name=user_data[0][1],
-                    first_name=user_data[0][2],
-                    email=mock_email,
-                    password=user_data[0][2],
-                    user_type=user_data[0][10],
-                    user_id=user_data[0][0]
-                )
-            except Exception as e:
-                print(e)
-                return Response("Error creating new user")
-            try:
-                for transaction in user_data:
-                    location = Location.objects.get(vendor_name=transaction[8])
-                    day = transaction[3]
-                    time = transaction[4]
-                    date_object = datetime.strptime(day + " " + time, '%m/%d/%y %H:%M')
-                    date_object = pytz.utc.localize(date_object)
-                    Transaction.objects.create(
-                        amount=transaction[6],
-                        category=transaction[7],
-                        payment_method=transaction[9],
-                        user=user,
-                        location=location,
-                        sale_date=date_object
-                    )
-            except Exception as e:
-                print(e)
-                return Response("Error creating transactions")
+		if request.method == 'POST':
+			user_data = generator.generate_new_user_data(user_type)
+			mock_email = user_data[0][1] + user_data[0][2] + "@gmail.com"
+			try:
+				user = User(
+					last_name=user_data[0][1],
+					first_name=user_data[0][2],
+					email=mock_email,
+					user_type=user_data[0][10],
+				)
+				user.set_password(user_data[0][2])
+				user.save()
+			except Exception as e:
+				print(e)
+				return Response("Error creating new user")
+			try:
+				for transaction in user_data:
+					location = Location.objects.get(vendor_name=transaction[8])
+					day = transaction[3]
+					time = transaction[4]
+					date_object = datetime.strptime(day + " " + time, '%m/%d/%y %H:%M')
+					date_object = pytz.utc.localize(date_object)
+					Transaction.objects.create(
+						amount=transaction[6],
+						category=transaction[7],
+						payment_method=transaction[9],
+						user=user,
+						location=location,
+						sale_date=date_object
+					)
+			except Exception as e:
+				print(e)
+				return Response("Error creating transactions")
 
-            return JsonResponse({"Success":
-                                     {'last_name':user_data[0][1],
-                                      'first_name':user_data[0][2],
-                                      'email':mock_email,
-                                      'password':user_data[0][2],
-                                      'user_type':user_data[0][10],
-                                      'user_id':user_data[0][0]}
-                                 })
+			return JsonResponse({"Success":
+				                     {'last_name' : user_data[0][1],
+				                      'first_name': user_data[0][2],
+				                      'email'     : mock_email,
+				                      'password'  : user_data[0][2],
+				                      'user_type' : user_data[0][10],
+				                      'user_id'   : user_data[0][0]}
+			                     })
